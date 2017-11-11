250e5fb4501ab573ab0a0d9d47435a7d460c783c
carlosalmonter <carlosalmonter@gmail.com>
2017-11-11 02:06:58 -0400

    adding config file

---
+++ src/config/config.js
@@ -0,0 +1,9 @@
+const config = {
+  API_URL: 'http://localhost:8080/api/',
+  POST_HEADERS: {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json',
+  }
+};
+
+export default config;
--- src/utils/Api.js
+++ src/utils/Api.js
@@ -1,17 +1,16 @@
-const apiUrl = 'http://localhost:8080/api/';
+import config from '../config/config';
+
+const apiUrl = config.API_URL;
 
 const post = (uri, params) => fetch(`${apiUrl}${uri}`, {
   method: 'post',
-  headers: {
-    'Accept': 'application/json',
-    'Content-Type': 'application/json',
-  },
+  headers: config.POST_HEADERS,
   body: JSON.stringify(params)
 })
   .then(response => response.json())
   .then(data => data);
 
-export const login = (email, password) => post(`authorize`, {
+export const login = (email, password) => post('authorize', {
   email: email,
   password: password
 });
